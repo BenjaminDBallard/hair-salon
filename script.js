bb33c4c5f66bda59f67c1ed06f65b3a9802b7456
BenjaminDBallard <benb971@gmail.com>
2022-10-20 01:04:35 -0500

    completed JS for dropdown menus, styled landing page

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-// Navbar
+// Navbar=============================================
 
 // Hightlights nav-item of the current page
 let current_url = document.location;
-document.querySelectorAll(".link-container .link").forEach(function(e){
+document.querySelectorAll(".link").forEach(function(e){
    if(e.href == current_url){
-      e.classList += " current";
+    e.classList = " link current";
    }
 });
+
+
+// onclick dropdown function 
+function dropdown() {
+    document.getElementById("nav-icon").classList.toggle("fa-xmark");
+    document.getElementById("nav-icon").classList.toggle("fa-bars");
+    if ( document.getElementById("link-container").classList.contains('wide-menu') ){
+        document.getElementById("link-container").classList = 'mobile-menu';
+    } else {
+        document.getElementById("link-container").classList = 'wide-menu';
+    }
+  }
+
+
+//   Close dropdown on resize 
+  window.addEventListener('resize', (e)=>{
+    if (window.innerWidth < "636px"){
+        document.getElementById("link-container").classList = 'mobile-menu';
+        document.getElementById("nav-icon").classList= "fa fa-bars";
+    } else {
+        document.getElementById("link-container").classList = 'wide-menu';
+        document.getElementById("nav-icon").classList= "fa fa-bars";
+    }
+  });
